af96f85c810739a253316a4d739f9d0bdce1c5bc
c <hoanglongmit@gmail.com>
2023-11-18 16:06:49 +0700

    use token authentication

--- ocelot/urls.py
+++ ocelot/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("rest_framework.urls")),
+    path("auth/token", views.obtain_auth_token),
     path("books/", include("books.urls")),
     path("users/", include("users.urls")),
 ]
