45f3c085bca149c2f9d83bb4f40765187491a45e
c <hoanglongmit@gmail.com>
2023-11-18 17:36:03 +0700

    configure api rate limit

--- ocelot/settings.py
+++ ocelot/settings.py
@@ -150,6 +150,14 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '1000000/day'
+    }
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
